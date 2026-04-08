7858254963ca576597d2a4d94a37550b50dbc2a6
dreadlord-runner <mayankyadav8285@gmail.com>
2026-04-08 22:30:13 +0530

    feat: enhance DeploymentPlatform component with additional server options and improved layout

--- forge-sample/src/components/DeploymentPlatform.jsx
+++ forge-sample/src/components/DeploymentPlatform.jsx
@@ -152,7 +152,46 @@ const DeploymentPlatform = () => {
             </div>
           </div>
 
-          {/* -----card 3--------- */}
+          {/* --------card 3--------- */}
+          <div className="p-10 md:p-14 border-b border-r border-gray-100 flex flex-col bg-white">
+            <h3 className="text-lg font-medium text-gray-900 mb-3">
+              Create any type of server{" "}
+            </h3>
+            <p className="text-gray-500 mb-12 leading-relaxed">
+              From app to database servers, provision everything <br /> with
+              ease. Single or networked, Forge can do it all.
+            </p>
+
+            <div className="relative pt-6 pl-6  mt-auto overflow-hidden">
+              <div className="space-y-2 pb-12 w-[150%] sm:w-full">
+                <div className="p-2 pl-3 border border-gray-200 rounded-lg bg-[#fbfcfb] shadow-sm hover:bg-gray-100">
+                  <div className=" text-gray-900 mb-1">App server</div>
+                  <div className="text-sm text-gray-500 truncate">
+                    A full-stack server, includes everything you need to run
+                    application
+                  </div>
+                </div>
+
+                <div className="p-2 pl-3 border border-gray-200 rounded-lg bg-white shadow-sm hover:bg-gray-100">
+                  <div className=" text-gray-900 mb-1">
+                    Load balancer server
+                  </div>
+                  <div className="text-sm text-gray-500">
+                    Distributes traffic across multiple servers.
+                  </div>
+                </div>
+
+                <div className="p-2 pl-3 border border-gray-200 rounded-lg bg-white shadow-sm opacity-50 hover:bg-gray-100">
+                  <div className=" text-gray-900 mb-1">Worker server</div>
+                  <div className="text-sm text-gray-500">
+                    A worker server that processes background jobs.
+                  </div>
+                </div>
+                <div className="absolute inset-y-0 right-0 w-32 bg-gradient-to-r from-transparent to-white pointer-events-none"></div>
+                <div className="absolute inset-x-0 bottom-0 h-32 bg-gradient-to-b from-transparent to-white pointer-events-none"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
